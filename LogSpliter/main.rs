04fae9680f2da1745560b365e8d87675ef8ca113
SwartzMss <swartz_lubel@outlook.com>
2024-09-13 23:56:44 +0800

    add tracing for async logging
    Signed-off-by: SwartzMss <swartz_lubel@outlook.com>

--- LogSpliter/main.rs
+++ LogSpliter/main.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
 use tokio::fs::File;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::fs;
+use std::time::Instant;
+use tracing::{info, error};
+use tracing_subscriber;
 
 #[derive(Parser)]
 #[command(name = "File Splitter")]
@@ -14,6 +18,9 @@ struct Args {
 }
 
 async fn split_file_by_lines(file_path: &str, lines_per_file: usize) -> io::Result<()> {
+    let metadata = fs::metadata(file_path).await?;
+    let file_size = metadata.len();
+    info!("File size: {} bytes", file_size);
     let file = File::open(file_path).await?;
     let reader = BufReader::new(file);
     let mut lines = reader.split(b'\n');
@@ -21,6 +28,8 @@ async fn split_file_by_lines(file_path: &str, lines_per_file: usize) -> io::Resu
     let mut file_count = 0;
     let mut line_count = 0;
     let mut current_file: Option<File> = None;
+
+    let start_time = Instant::now();
 
     while let Some(line) = lines.next_segment().await? {
         if line_count % lines_per_file == 0 {
@@ -37,15 +46,26 @@ async fn split_file_by_lines(file_path: &str, lines_per_file: usize) -> io::Resu
         line_count += 1;
     }
 
+    info!("Total split files created: {}", file_count);
+    info!("Total lines processed: {}", line_count);
+
+    let duration = start_time.elapsed();
+    info!("File splitting completed in: {:.2?}", duration);
+    info!("Split job has been done!");
+
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter("info")
+        .with_ansi(false)
+        .init();
+
     let args = Args::parse();
 
     if let Err(e) = split_file_by_lines(&args.file, args.lines).await {
-        eprintln!("Error occurred: {}", e);
+        error!("Error occurred: {}", e);
     }
 }
-
